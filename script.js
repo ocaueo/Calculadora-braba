a69dad9b51dafc4bae25ef695e693cd6f9011ba8
ocaueo <caue4824@gmail.com>
2023-06-15 10:25:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let resposta = document.querySelector(".resposta");
+
+btn.onclick = function(){
+    let a = altura.value;
+    let p = peso.value;
+    
+    let calculo = p / (a * a);
+    console.log(calculo);
+    resposta.innerText = `Seu IMC é: ${calculo.toFixed(2)}`;
+    
+    if(calculo <= 18.4){ resposta.innerText += ` Abaixo do peso`; return} ;
+    if(calculo <= 24.9){ resposta.innerText += ` Peso ideal`; return};
+    if(calculo <= 29.9){ resposta.innerText += ` Excesso de peso`; return};
+    if(calculo <= 34.9){ resposta.innerText += ` Obesidade 1`; return};
+    if(calculo <= 39.9){ resposta.innerText += ` Obesidade 2`; return};
+}
