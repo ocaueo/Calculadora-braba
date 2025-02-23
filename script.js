59235a90d592b9e9ed732bff57239417be4d0c4d
unknown <caue4824@gmail.com>
2025-02-23 03:48:49 -0300

    Uma Leve Atualização no Projeto

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ btn.onclick = function(){
     console.log(calculo);
     resposta.innerText = `Seu IMC é: ${calculo.toFixed(2)}`;
     
-    if(calculo <= 18.4){ resposta.innerText += ` Abaixo do peso`; return} ;
-    if(calculo <= 24.9){ resposta.innerText += ` Peso ideal`; return};
-    if(calculo <= 29.9){ resposta.innerText += ` Excesso de peso`; return};
+    if(calculo <= 18.4){ resposta.innerText += ` Abaixo do Peso`; return};
+    if(calculo <= 24.9){ resposta.innerText += ` Peso Ideal`; return};
+    if(calculo <= 29.9){ resposta.innerText += ` Excesso de Peso`; return};
     if(calculo <= 34.9){ resposta.innerText += ` Obesidade 1`; return};
     if(calculo <= 39.9){ resposta.innerText += ` Obesidade 2`; return};
 }
